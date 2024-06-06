5d30e4ec02875b742b899be48ea39dd6403db361
Aditya Srivastava <97466527+Deetu8346@users.noreply.github.com>
2024-06-06 16:52:04 +0530

    Update Initializations.c

--- Initializations.c
+++ Initializations.c
@@ -3,7 +3,6 @@
 #include<fcntl.h>
 #include<string.h>
 
-//Basic Structures for different types of Users
 
 typedef struct normalUser
 {
@@ -33,13 +32,12 @@ typedef struct admin
 	char password[10];
 }admin;
 
-//Initialising the Databases
+
 
 int main()
 {
-	int fd=open("Admindb",O_RDWR | O_CREAT,0744); // system call 
-	// Admindb name ki file create ho jati hai ...in read write mode or if this file does not exist then it is created 
-	int choice ; int id = 10000; //Starting value of the ids
+	int fd=open("Admindb",O_RDWR | O_CREAT,0744); 
+	int choice ; int id = 10000; 
 	admin admin;
 	printf("Enter the name of the admin: ");
 	scanf(" %s",admin.username);
@@ -47,10 +45,7 @@ int main()
 	scanf(" %s",admin.password);
 	admin.userID=id++;
 	printf("Your userID is : %d\n",admin.userID);
-	write(fd,&admin,sizeof(admin)); // system call
-	// fd: file descriptor
-	// admin: buffer to write data from.
-	//  sizeof(admin): length of the buffer
+	write(fd,&admin,sizeof(admin)); 
 	printf("Do you want to create More Admins(1/0)?\n");
 	scanf("%d",&choice);
 	while(choice == 1)
@@ -61,15 +56,15 @@ int main()
 		scanf(" %[^\n]", admin.password);
 		admin.userID=id++;
 		printf("Your userID is : %d\n",admin.userID);
-		write(fd,&admin,sizeof(admin)); //New Admin Written // system call
+		write(fd,&admin,sizeof(admin)); 
 
 		printf("Do you want to create More Admins(1/0)? \n");
 		scanf("%d",&choice);
 	}
-	close(fd); // system call
+	close(fd);
 
-	//Same procedure has been followed for NormalUser and JOintUser
-	fd=open("NormalUserdb",O_RDWR | O_CREAT,0744);// system call
+	
+	fd=open("NormalUserdb",O_RDWR | O_CREAT,0744);
 	choice = 'Y'; id = 10000;
 	normalUser NU;
 	printf("Enter the name of the normal user: ");
@@ -77,11 +72,11 @@ int main()
 	printf("Enter password: ");
 	scanf(" %[^\n]",NU.password);
 	NU.userID=id++;
-	NU.balance=9999; //Initial Balance
-	NU.account_no=(NU.userID-10000)+1000000; //Accountid has been derived from userid for simplicity
+	NU.balance=9999;
+	NU.account_no=(NU.userID-10000)+1000000; 
 	printf("Your userID is : %d\n",NU.userID);
 	strcpy(NU.status,"ACTIVE");
-	write(fd,&NU,sizeof(normalUser));// system call
+	write(fd,&NU,sizeof(normalUser));
 	printf("Do you want to create More NormalUser Accounts(1/0)? \n");
 	scanf("%d",&choice);
 	while(choice == 1)
@@ -95,13 +90,13 @@ int main()
 		NU.account_no=(NU.userID-10000)+1000000;
 		printf("Your userID is : %d\n",NU.userID);
 		strcpy(NU.status,"ACTIVE");
-		write(fd,&NU,sizeof(normalUser));// system call
+		write(fd,&NU,sizeof(normalUser));
 		printf("Do you want to create More NormalUser Accounts(1/0)? \n");
 		scanf("%d",&choice);
 	}
-	close(fd);// system call
+	close(fd);
 
-	fd=open("JointUserdb",O_RDWR | O_CREAT,0744);// system call
+	fd=open("JointUserdb",O_RDWR | O_CREAT,0744);
 	choice='Y'; id = 10000;
 	jointUser JU;
 	printf("Enter the main name of the joint user: ");
@@ -115,7 +110,7 @@ int main()
 	JU.account_no=(JU.userID-10000)+1000000;
 	printf("Your userID is : %d\n",JU.userID);
 	strcpy(JU.status,"ACTIVE");
-	write(fd,&JU,sizeof(jointUser));// system call
+	write(fd,&JU,sizeof(jointUser));
 	printf("Do you want to create More JointUser Accounts(1/0)? \n");
 	scanf("%d",&choice);
 	while(choice == 1)
@@ -131,10 +126,10 @@ int main()
 		JU.account_no=(JU.userID-10000)+1000000;
 		printf("Your userID is : %d\n",JU.userID);
 		strcpy(JU.status,"ACTIVE");
-		write(fd,&JU,sizeof(jointUser));// system call
+		write(fd,&JU,sizeof(jointUser));
 		printf("Do you want to create More JointUser Accounts(1/0)? \n");
 		scanf("%d",&choice);
 	}
-	close(fd);// system call
+	close(fd);
 	return 0;
 }
